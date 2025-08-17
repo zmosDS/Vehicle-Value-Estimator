56f5d40c076a26c4f8edb9eda4034d2a7af5dc29
zmosDS <122930289+zmosDS@users.noreply.github.com>
2025-08-17 16:17:05 -0700

    Update estimator_dashboard.py

--- estimator_dashboard/estimator_dashboard.py
+++ estimator_dashboard/estimator_dashboard.py
@@ -6,23 +6,23 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load both datasets and combine for biggest range of features
-julyData = pd.read_csv('/Users/z/Library/Mobile Documents/com~apple~CloudDocs/DATA SCIENCE PROJECTS/Cars/Github/cleaned_data_july_21st.csv')
-augustData = pd.read_csv('/Users/z/Library/Mobile Documents/com~apple~CloudDocs/DATA SCIENCE PROJECTS/Cars/Github/cleaned_data_aug_16th.csv')
-df_cars = pd.concat([julyData, augustData], ignore_index=True)
+julyData   = pd.read_csv(REPO_ROOT/'data'/'cleaned_data_july_21st.csv')
+augustData = pd.read_csv(REPO_ROOT/'data'/'cleaned_data_aug_16th.csv')
+df_cars    = pd.concat([julyData, augustData], ignore_index=True)
 
 # Capitalize the first letter of the make, model, trim, and body style
-df_cars['Make'] = df_cars['Make'].str.title()
-df_cars['Model'] = df_cars['Model'].str.title()
-df_cars['Trim'] = df_cars['Trim'].str.title()
+df_cars['Make']       = df_cars['Make'].str.title()
+df_cars['Model']      = df_cars['Model'].str.title()
+df_cars['Trim']       = df_cars['Trim'].str.title()
 df_cars['Body Style'] = df_cars['Body Style'].str.title()
 
 # Sort unique values for make and state
-unique_makes = sorted(df_cars['Make'].unique())
+unique_makes  = sorted(df_cars['Make'].unique())
 unique_states = sorted(df_cars['State'].unique())
 
 # Load model and scaler
-final_model = joblib.load('/Users/z/Library/Mobile Documents/com~apple~CloudDocs/DATA SCIENCE PROJECTS/Cars/Github/final_model.pkl')
-scaler = joblib.load('/Users/z/Library/Mobile Documents/com~apple~CloudDocs/DATA SCIENCE PROJECTS/Cars/Github/final_scaler.pkl')
+final_model = joblib.load(REPO_ROOT / "models" / "final_model.joblib")
+scaler      = joblib.load(REPO_ROOT / "models" / "final_model.joblib")
 
 # Welcome message on sidebar
 st.sidebar.title('Welcome to the Vehicle Value Estimator!')
@@ -43,17 +43,17 @@ with col1:
     
     # Model, filter based on make
     filtered_models = df_cars[df_cars['Make'] == make]['Model'].unique() if make else []
-    model_name = st.selectbox('Model', sorted(filtered_models), disabled=not make)
+    model_name      = st.selectbox('Model', sorted(filtered_models), disabled=not make)
 
     # Trim, filter based on model
     filtered_trims = df_cars[df_cars['Model'] == model_name]['Trim'].unique() if model_name else []
-    trim = st.selectbox('Trim', sorted(filtered_trims), disabled=not model_name)
+    trim           = st.selectbox('Trim', sorted(filtered_trims), disabled=not model_name)
 
 with col2:
 
     # Body style, filter based on model and trim 
     filtered_body_styles = df_cars[(df_cars['Model'] == model_name) & (df_cars['Trim'] == trim)]['Body Style'].unique() if (model_name and trim) else []
-    body_style = st.selectbox('Body Style', sorted(filtered_body_styles), disabled=not (model_name and trim))
+    body_style           = st.selectbox('Body Style', sorted(filtered_body_styles), disabled=not (model_name and trim))
 
     # Mileage
     mileage = st.number_input('Mileage', min_value=1000, max_value=300000, value=25000)
@@ -66,17 +66,17 @@ year = st.slider('Year', min_value=2010, max_value=2024, value=2012)
 
 # Define dataframe from user inputs
 input_data = pd.DataFrame({
-    'Year': [year],
-    'Mileage': [mileage],
-    'Trim': [trim],
-    'Model': [model_name],
-    'State': [state],
-    'Make': [make],
+    'Year':       [year],
+    'Mileage':    [mileage],
+    'Trim':       [trim],
+    'Model':      [model_name],
+    'State':      [state],
+    'Make':       [make],
     'Body Style': [body_style],
 })
 
 # Ensure categorical columns are 'category'
-cat_columns = ['Model', 'State', 'Trim', 'Make', 'Body Style']
+cat_columns             = ['Model', 'State', 'Trim', 'Make', 'Body Style']
 input_data[cat_columns] = input_data[cat_columns].astype('category')
 
 # Scale numerical features
@@ -88,7 +88,7 @@ st.write(f'### Estimated Value: ${predicted_price:,.2f}')
 
 # Price distribution for similar cars
 st.write('### Price Distribution for Similar Vehicles')
-similar_cars = df_cars[(df_cars['Make'] == make) & (df_cars['Model'] == model_name) & (df_cars['Trim'] == trim) & (df_cars['Body Style'] == body_style)]
+similar_cars       = df_cars[(df_cars['Make'] == make) & (df_cars['Model'] == model_name) & (df_cars['Trim'] == trim) & (df_cars['Body Style'] == body_style)]
 price_distribution = similar_cars['Price']
 
 # Plot price distribution for similar cars
@@ -102,7 +102,7 @@ st.pyplot(plt)
 st.write('### Feature Importance')
 
 # Extract feature importances
-importance = final_model.feature_importances_
+importance    = final_model.feature_importances_
 importance_df = pd.DataFrame({'Feature': input_data.columns, 'Importance': importance}).sort_values(by='Importance', ascending=False)
 
 # Plot feature importances
